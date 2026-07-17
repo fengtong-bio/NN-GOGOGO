23017d0533ba88a4509b89f646b00e2f7a77b0e0
fengtong <38370662+fengtong-bio@users.noreply.github.com>
2026-07-17 22:01:32 +0800

    Update prokka_emapper_dbcan.sh

--- prokka_emapper_dbcan.sh
+++ prokka_emapper_dbcan.sh
@@ -22,4 +22,7 @@ run_dbcan --out_dir /PATH/MAG_cazy/$i --db_dir /PATH/software/cazy_db --hmm_cpu 
 export PATH=/PATH/mamba/mambaforge/envs/dbcan4/bin:$PATH
 mkdir /PATH/MAG_dbCAN/$i
 run_dbcan --out_dir /PATH/MAG_dbCAN/$i --db_dir /PATH/MAG_prokka/$i/$i.faa protein --tools dbcansub --dbcan_thread 1
+
+run_dbcan easy_substrate --input_raw_data /PATH/MAG_prokka/$i/$i.faa --mode protein --output_dir /PATH/MAG_dbCAN/$i --db_dir /PATH/software/SK_conda/CAZy_dbcan_database --input_gff /PATH/MAG_prokka/$i/$i.faa --gff_type prodigal --hmm_cpu 2 --dbcan_thread 2 --log-level INFO
+
 done
